f04b9cf963f7fc4138f0e6682353cea63fc9c241
frytolnacestach <frytolnacestach@gmail.com>
2022-11-17 11:42:43 +0100

    link in nuxt

--- components/MenuMain.vue
+++ components/MenuMain.vue
@@ -9,7 +9,7 @@
                                 <div class="m-nav-main__inner">
                                     <ul class="m-nav-main__items">
                                         <li class="m-nav-main__item">
-                                            <a class="m-nav-main__link" href="/admin">Administrace</a>
+                                            <NuxtLink class="m-nav-main__link" to="/admin">Administrace</NuxtLink>
                                         </li>
                                     </ul>
                                 </div>
