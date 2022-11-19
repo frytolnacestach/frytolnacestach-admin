d7b39fd378eddc030f069a32cc86772662797184
frytolnacestach <frytolnacestach@gmail.com>
2022-11-19 02:05:58 +0100

    Static version

---
+++ dist/_nuxt/static/1668819458/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flogout","\u002Fadmin","\u002Flogin","\u002Fadmin\u002Fpost","\u002Fadmin\u002Fpost\u002Ftrikrizovy-vrch","\u002Fadmin\u002Fpost\u002Fsvet-meduz"]})
