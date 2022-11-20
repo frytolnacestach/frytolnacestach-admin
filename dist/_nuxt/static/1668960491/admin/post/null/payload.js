7710453c108cbcc67edb528f54425385115eb395
frytolnacestach <frytolnacestach@gmail.com>
2022-11-20 17:08:30 +0100

    Static version

---
+++ dist/_nuxt/static/1668960491/admin/post/null/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/admin/post/null", {data:[{post:[]}],fetch:{},mutations:void 0});
---
+++ dist/_nuxt/static/1668960491/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fadmin","\u002Flogout","\u002Flogin","\u002Fadmin\u002Fpost","\u002Fadmin\u002Fpost\u002Fsvet-meduz","\u002Fadmin\u002Fpost\u002Ftrikrizovy-vrch","\u002Fadmin\u002Fpost\u002Fnull"]})
