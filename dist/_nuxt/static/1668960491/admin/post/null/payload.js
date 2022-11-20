00947330615dc2d65d60061b8519ea8b6078f939
frytolnacestach <frytolnacestach@gmail.com>
2022-11-20 22:30:55 +0100

    Static version

--- dist/_nuxt/static/1668960491/admin/post/null/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/admin/post/null", {data:[{post:[]}],fetch:{},mutations:void 0});
---
+++ dist/_nuxt/static/1668979826/admin/post/create/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668979826",layout:"default",error:a,serverRendered:true,routePath:"\u002Fadmin\u002Fpost\u002Fcreate",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
