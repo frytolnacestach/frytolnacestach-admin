f8a6b14aaf1059d428fb448ce919e737db1ebb19
frytolnacestach <frytolnacestach@gmail.com>
2022-11-23 21:33:36 +0100

    base form

--- pages/admin/base/index.vue
+++ pages/admin/base/index.vue
@@ -24,6 +24,57 @@
                 </div>
             </div>
         </nav>
+
+        <section class="t-section my-4">
+            <div class="t-section__inner">
+
+                <div class="o-form-edit">
+                    <div class="o-form-edit__outer">
+                        <div class="o-form-edit__inner">
+                            
+                            <div class="o-flash-messages" v-if="errorForm">
+                                <div class="o-flash-messages__items">
+                                    <div class="o-flash-messages__item">
+                                        <div class="o-flash-messages__outer">
+                                            <div class="o-flash-messages__inner">
+                                                <span class="o-flash-messages__text">{{ errorForm }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <form class="o-form-edit__form" @submit.prevent="editForm">
+                                <div class="o-form-edit__items">
+                                    
+                                    <div class="o-form-edit__item">
+                                        <label class="m-label">
+                                            <span class="m-label__name">Kdo jsem:</span>
+                                        </label>
+                                        <input class="a-input" type="text" name="iam" v-model="edit.iam" />
+                                    </div>
+
+                                    <div class="o-form-edit__item">
+                                        <label class="m-label">
+                                            <span class="m-label__name">JAK MĚ PODPOŘIT:</span>
+                                        </label>
+                                        <input class="a-input" type="text" name="donate" v-model="edit.donate" />
+                                    </div>
+
+                                </div>
+                                <div class="o-form-edit__buttons mt-1">
+                                    <div class="o-form-edit__button">
+                                        <div class="m-button">
+                                            <button class="m-button__input" type="submit">Uložit úpravy</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -32,6 +83,41 @@
 export default {
     name: 'AdminBasePage',
 
+    data() {
+        return {
+            edit: {
+                iam: '',
+                donate: '',
+            },
+            errorForm: '',
+            base: ''
+        }
+    },
+    methods: {
+            async editForm(){
+                try {
+                    let result = await axios.post(`https://frytolnacestach-api.vercel.app/api/base-edit`, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Headers": "x-access-token, Origin, Content-Type, Accept",
+                        },
+                        method: 'POST',
+                        body: {
+                            'iam': this.edit.iam,
+                            'donate': this.edit.donate,
+                        }
+                    })
+                    .then(function (response) {
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        },
     mounted() {
         let user = localStorage.getItem('user-info')
 
@@ -40,6 +126,9 @@ export default {
         } else {
             this.$router.push('login')
         }
+
+        this.edit.iam = this.post[0].iam
+        this.edit.donate = this.post[0].donate
     },
     async asyncData({ $axios }) {
         const base = await $axios.$get(`https://frytolnacestach-api.vercel.app/api/base`)
