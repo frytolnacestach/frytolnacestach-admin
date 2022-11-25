ac8c0e80e554c14c405b1bcc19fd62ac71933c2e
frytolnacestach <frytolnacestach@gmail.com>
2022-11-25 02:17:59 +0100

    Static version

--- dist/_nuxt/static/1669338803/admin/platforms/youtube/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669338803",layout:"default",error:a,serverRendered:true,routePath:"\u002Fadmin\u002Fplatforms\u002Fyoutube",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
