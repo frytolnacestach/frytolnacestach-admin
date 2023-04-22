bb461a951140611aad1d5a470bab8fd55fd70ecf
frytolnacestach <frytolnacestach@gmail.com>
2023-04-22 10:00:12 +0200

    Update t-header

--- assets/js/js_m-hamburger.js
+++ assets/js/js_m-hamburger.js
@@ -5,13 +5,13 @@ $( document ).ready(function() {
         if ($status == "close") {
             $(this).data('hamburger', 'open');
             $(this).addClass('open');
-            $(".t-header").addClass('open');
+            $(".js_m-nav-main").addClass('open');
             $("html").addClass('no-scroll');
             $("body").addClass('no-scroll');
         } else {
             $(this).data('hamburger', 'close');
             $(this).removeClass('open');
-            $(".t-header").removeClass('open');
+            $(".js_m-nav-main").removeClass('open');
             $("html").removeClass('no-scroll');
             $("body").removeClass('no-scroll');
         }
@@ -20,7 +20,7 @@ $( document ).ready(function() {
     $( ".js_m-nav-main__link" ).click(function() {
         $( ".js_m-hamburger" ).data('hamburger', 'close');
         $( ".js_m-hamburger" ).removeClass('open');
-        $(".t-header").removeClass('open');
+        $(".js_m-nav-main").removeClass('open');
         $("html").removeClass('no-scroll');
         $("body").removeClass('no-scroll');
     });
--- components/molecules/mNavMain.vue
+++ components/molecules/mNavMain.vue
@@ -1,28 +1,18 @@
 <template>
-    <header class="t-header">
-        <div class="o-header">
-            <div class="o-header__outer">
-                <div class="o-header__inner">
-                    <div class="o-header__nav">
-                        <nav class="m-nav-main">
-                            <div class="m-nav-main__outer">
-                                <div class="m-nav-main__inner">
-                                    <ul class="m-nav-main__items">
-                                        <li class="m-nav-main__item">
-                                            <NuxtLink class="m-nav-main__link" to="/admin">Administrace</NuxtLink>
-                                        </li>
-                                        <li class="m-nav-main__item" v-if="loginStatus">
-                                            <NuxtLink class="m-nav-main__link" to="/logout">Odhlásit se</NuxtLink>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
+    <nav class="m-nav-main">
+        <div class="m-nav-main__outer">
+            <div class="m-nav-main__inner">
+                <ul class="m-nav-main__items">
+                    <li class="m-nav-main__item">
+                        <NuxtLink class="m-nav-main__link" to="/admin">Administrace</NuxtLink>
+                    </li>
+                    <li class="m-nav-main__item" v-if="loginStatus">
+                        <NuxtLink class="m-nav-main__link" to="/logout">Odhlásit se</NuxtLink>
+                    </li>
+                </ul>
             </div>
         </div>
-    </header>
+    </nav>
 </template>
 
 <script>
