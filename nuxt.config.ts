1d2e547839c09a6fb1a781e7bb96ec9f2240322f
frytolnacestach <frytolnacestach@gmail.com>
2023-06-13 21:52:11 +0200

    Nuxt update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,34 @@
-import type { NuxtConfig } from '@nuxt/types'
-
-const config: NuxtConfig = {
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Administrace Frytol na cestách',
-    htmlAttrs: {
-      lang: 'cs'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Administrace Frytol na cestách' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'msapplication-TileColor', content: '#da532c' },
-      { name: 'theme-color', content: '#ffffff' },
-      { name: 'author', content: 'Michal Fryč' },
-      { name: 'keywords', content: 'Administrace Frytol na cestách' },
-      { name: 'robots', content: 'index, follow' },
-    ],
-    link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicons/apple-touch-icon.png' },
-      { rel: 'icon', type:'image/png', sizes: '32x32', href: '/images/favicons/favicon-32x32.png' },
-      { rel: 'icon', type:'image/png', sizes: '16x16', href: '/images/favicons/favicon-16x16.png' },
-      { rel: 'manifest', sizes: '180x180', href: '/images/favicons/site.webmanifest' }
-    ],
-    script: [
-      { src: 'https://code.jquery.com/jquery-3.6.1.min.js', crossorigin: 'anonymous'},
-      { src: '/js/js_m-hamburger.js', type: 'text/javascript', defer: 'defer' },
-      { src: '/js/js_o-cookies-dialog.js', type: 'text/javascript', defer: 'defer' }
-    ]
+  app: {
+    head: {
+      title: 'Administrace Frytol na cestách',
+      htmlAttrs: {
+        lang: 'cs'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Administrace Frytol na cestách' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
+        { name: 'author', content: 'Michal Fryč' },
+        { name: 'keywords', content: 'Administrace Frytol na cestách' },
+        { name: 'robots', content: 'index, follow' },
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicons/apple-touch-icon.png' },
+        { rel: 'icon', type:'image/png', sizes: '32x32', href: '/images/favicons/favicon-32x32.png' },
+        { rel: 'icon', type:'image/png', sizes: '16x16', href: '/images/favicons/favicon-16x16.png' },
+        { rel: 'manifest', sizes: '180x180', href: '/images/favicons/site.webmanifest' }
+      ],
+      script: [
+        { src: '/js/js_m-hamburger.js', type: 'text/javascript', async: true },
+      ]
+    }
   },
 
   runtimeConfig: {
@@ -43,35 +41,6 @@ const config: NuxtConfig = {
     '@/assets/css/main.scss'
   ],
 
-  plugins: [
-    '~/plugins/sharp.js'
-  ],
+  devtools: { enabled: true }
+})
 
-  modules: [
-    '@nuxt/image-edge'
-  ],
-
-  env: {
-    staticDir: process.env.NODE_ENV === 'production' ? 'static' : '',
-  },
-  
-  buildModules: [
-    '@nuxt/image'
-  ],
-
-  vue: {
-    config: {
-      productionTip: false,
-      devtools: true,
-    },
-  },
-
-  image: {
-    providers: {
-      local: {
-        provider: 'sharp',
-      },
-    },
-  },
-}
-export default config
--- plugins/sharp.js
+++
@@ -1,6 +0,0 @@
-const sharp = require('sharp')
-
-export default function (context, inject) {
-  // Inject `sharp` library as `$sharp` global variable
-  inject('sharp', sharp)
-}
