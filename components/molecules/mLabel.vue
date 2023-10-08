0b3197d59aefc23908c8974e64408124cb15c500
frytolnacestach <frytolnacestach@gmail.com>
2023-10-08 15:18:21 +0200

    Create component mLabel

---
+++ components/molecules/mLabel.vue
@@ -0,0 +1,35 @@
+<template>
+    <label class="m-label">
+        <span class="m-label__name">
+            {{ name }}
+            <span class="m-label__name-column" v-if="nameDB"> ({{ nameDB }})</span>
+            <span class="m-label__name-required" v-if="required">*</span>
+        </span>
+        <span class="m-label__perex" v-if="perex">{{ perex }}</span>
+    </label>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmLabelComponent',
+
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            nameDB: {
+                type: String,
+                required: false
+            },
+            perex: {
+                type: String,
+                required: false
+            },
+            required: {
+                type: Boolean,
+                required: false
+            }
+        }
+    })
+</script>
