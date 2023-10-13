a8512f2e84010668cead1c5bdd3bac9fe5159479
frytolnacestach <frytolnacestach@gmail.com>
2023-10-13 15:53:06 +0200

    Create Component mInputIDPlaces

---
+++ components/molecules/mInputIDPlaces.vue
@@ -0,0 +1,37 @@
+<template>
+    <input class="a-input" type="number" min="0" :name="name" v-model="id" :required="required" />
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmInputIDPlaceComponent',
+
+        props: {
+            value: {
+                type: Number,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            required: {
+                type: Boolean,
+                required: false
+            }
+        },
+
+        data() {
+            return {
+                id: this.value
+            }
+        },
+
+        watch: {
+            // Change image ID
+            id(newValue, oldValue) {
+                this.$emit('id-place', this.id)
+            }
+        },
+    })
+</script>
