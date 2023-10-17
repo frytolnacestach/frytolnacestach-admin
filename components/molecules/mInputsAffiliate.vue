0ca1fd8eec428e832ef7db99dd61ca2630c8050c
frytolnacestach <frytolnacestach@gmail.com>
2023-10-17 23:30:25 +0200

    Create Component mInputsAffiliate

---
+++ components/molecules/mInputsAffiliate.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="o-form-item__group">
+        <div class="o-form-item__group-items">
+            <div class="o-form-item__group-item" v-for="(item, index) in affiliateArray" :key="index">
+                <div class="m-button-remove">
+                    <button class="m-button-remove__input" type="button" @click="removeAffiliateInput(index)">
+                        Odstranit
+                    </button>
+                </div>
+                <div class="o-form-item__group-inputs">
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Name:</label>
+                        <input class="a-input" type="text" v-model="item.name" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Value:</label>
+                        <input class="a-input" type="text" v-model="item.value" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="o-form-item__buttons mt-1">
+            <div class="o-form-item__button">
+                <div class="m-button-add">
+                    <button class="m-button-add__input" type="button" @click="addAffiliateInput">Přidat affileate</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmInputsAffiliateComponent',
+
+        props: {
+            value: {
+                type: Array,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                affiliateArray: JSON.parse(this.value)
+            }
+        },
+
+        methods: {
+            addAffiliateInput() {
+                this.affiliateArray.push({
+                    name: '',
+                    value: true
+                })
+                this.$emit('affiliate', this.affiliateArray)
+            },
+            removeAffiliateInput(index) {
+                this.affiliateArray.splice(index, 1)
+            }
+        },
+
+        watch: {
+            value(newValue, oldValue) {
+                this.affiliateArray = JSON.parse(this.value)
+            },
+            affiliateArray: {
+                deep: true,
+                handler(newValue, oldValue) {
+                    this.$emit('affiliate', this.affiliateArray)
+                }
+            }
+        }
+    })
+</script>
