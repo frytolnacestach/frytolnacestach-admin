33d302e67228f08409ce9566abc45530c1ec6b2c
frytolnacestach <frytolnacestach@gmail.com>
2023-10-22 05:12:41 +0200

    Create Component mInputsVisitorsEntry

---
+++ components/molecules/mInputsVisitorsEntry.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="o-form-item__group">
+        <div class="o-form-item__group-items">
+            <div class="o-form-item__group-item" v-for="(item, index) in visitorsEntryArray" :key="index">
+                <div class="m-button-remove">
+                    <button class="m-button-remove__input" type="button" @click="removeVisitorsEntryInput(index)">
+                        Odstranit
+                    </button>
+                </div>
+                <div class="o-form-item__group-inputs">
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Name:</label>
+                        <input class="a-input" type="text" v-model="item.name" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Value:</label>
+                        <input class="a-input" type="text" v-model="item.value" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Date create:</label>
+                        <input class="a-input" type="text" v-model="item.date_create" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Date update:</label>
+                        <input class="a-input" type="text" v-model="item.date_update" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Author create:</label>
+                        <input class="a-input" type="text" v-model="item.author_create" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Author update:</label>
+                        <input class="a-input" type="text" v-model="item.author_update" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="o-form-item__buttons mt-1">
+            <div class="o-form-item__button">
+                <div class="m-button-add">
+                    <button class="m-button-add__input" type="button" @click="addVisitorsEntryInput">Přidat podmínku</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmInputsVisitorsEntryComponent',
+
+        props: {
+            value: {
+                type: Array,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                visitorsEntryArray: this.isValidJSON(this.value) ? JSON.parse(this.value) : []
+            }
+        },
+
+        methods: {
+            isValidJSON(value) {
+                try {
+                    JSON.parse(value)
+                    return true
+                } catch (e) {
+                    return false
+                }
+            },
+            addVisitorsEntryInput() {
+                this.visitorsEntryArray.push({
+                    name: '',
+                    value: '',
+                    date_create: '',
+                    date_update: '',
+                    author_create: null,
+                    author_update: null
+                })
+                this.$emit('visitors-entry', this.visitorsEntryArray)
+            },
+            removeVisitorsEntryInput(index) {
+                this.visitorsEntryArray.splice(index, 1)
+            }
+        },
+
+        watch: {
+            value(newValue, oldValue) {
+                this.visitorsEntryArray = JSON.parse(this.value)
+            },
+            visitorsEntryArray: {
+                deep: true,
+                handler(newValue, oldValue) {
+                    this.$emit('visitors-entry', this.visitorsEntryArray)
+                }
+            }
+        }
+    })
+</script>
