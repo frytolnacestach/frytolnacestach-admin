2c599fbd5c5c6997015289e296375980eda30245
frytolnacestach <frytolnacestach@gmail.com>
2023-10-25 22:56:11 +0200

    Create Components mInputsFacts

---
+++ components/molecules/mInputsFacts.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="o-form-item__group">
+        <div class="o-form-item__group-items">
+            <div class="o-form-item__group-item" v-for="(item, index) in factsArray" :key="index">
+                <div class="m-button-remove">
+                    <button class="m-button-remove__input" type="button" @click="removeFactInput(index)">
+                        Odstranit
+                    </button>
+                </div>
+                <div class="o-form-item__group-inputs">
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Name:</label>
+                        <input class="a-input" type="text" v-model="item.name" />
+                    </div>
+                    <div class="o-form-item__group-input">
+                        <label class="m-label">Value:</label>
+                        <input class="a-input" type="text" v-model="item.value" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="o-form-item__buttons mt-1">
+            <div class="o-form-item__button">
+                <div class="m-button-add">
+                    <button class="m-button-add__input" type="button" @click="addFactInput">Přidat fakt</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import mLabel from '@/components/molecules/mLabel.vue'
+
+    export default defineComponent({
+        name: 'MoleculesmInputsTagsComponent',
+
+        //COMPONENTS
+        components: {
+            mLabel
+        },
+
+        props: {
+            value: {
+                type: Array,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                factsArray: this.isValidJSON(this.value) ? JSON.parse(this.value) : []
+            }
+        },
+
+        methods: {
+            isValidJSON(value) {
+                try {
+                    JSON.parse(value)
+                    return true
+                } catch (e) {
+                    return false
+                }
+            },
+            addFactInput() {
+                this.factsArray.push({
+                    name: ''
+                })
+                this.$emit('facts', this.factsArray)
+            },
+            removeFactInput(index) {
+                this.factsArray.splice(index, 1)
+            }
+        },
+
+        watch: {
+            value(newValue, oldValue) {
+                this.factsArray = JSON.parse(this.value)
+            },
+            factsArray: {
+                deep: true,
+                handler(newValue, oldValue) {
+                    this.$emit('facts', this.factsArray)
+                }
+            }
+        }
+    })
+</script>
