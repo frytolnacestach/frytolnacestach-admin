28dbb8c826aed378aa5a956bd067be2a09302aca
frytolnacestach <frytolnacestach@gmail.com>
2023-12-21 00:38:19 +0100

    Add Component aTextarea

---
+++ components/atoms/aTextarea.vue
@@ -0,0 +1,37 @@
+<template>
+    <textarea class="a-textarea" type="text" :name="name" v-model="textareaValue" :required="required"></textarea>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'AtomsmTextareaComponent',
+
+        props: {
+            value: {
+                type: Number,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            required: {
+                type: Boolean,
+                required: false
+            }
+        },
+
+        data() {
+            return {
+                textareaValue: this.value
+            }
+        },
+
+        watch: {
+            // Change value
+            textareaValue(newValue, oldValue) {
+                this.$emit('textareaValue', this.textareaValue)
+            }
+        }
+    })
+</script>
