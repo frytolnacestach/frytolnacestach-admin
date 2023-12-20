b477ada85934b3f3a5ea8b66b850325f830949b0
frytolnacestach <frytolnacestach@gmail.com>
2023-12-21 00:24:01 +0100

    Add Component mInput

---
+++ components/molecules/mInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <input class="a-input" :name="name" v-model="inputValue" :required="required" />
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmInputComponent',
+
+        props: {
+            value: {
+                type: Number,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            required: {
+                type: Boolean,
+                required: false
+            }
+        },
+
+        data() {
+            return {
+                inputValue: this.value
+            }
+        },
+
+        watch: {
+            // Change image ID
+            inputValue(newValue, oldValue) {
+                this.$emit('inputValue', this.inputValue)
+            }
+        }
+    })
+</script>
