fce367682bcb9ba0676a437c1c9c42386b719b99
frytolnacestach <frytolnacestach@gmail.com>
2023-12-28 16:54:39 +0100

    check Image raw webp

--- pages/admin/images/[id].vue
+++ pages/admin/images/[id].vue
@@ -119,7 +119,7 @@
                 <div class="t-section__inner">
                     <div class="flex flex-center">
                         <div class="o-box -w640 -center -gray -text-center">
-                            <span :class="'a-button-file ' + (webP ? '-existing' : '-no-existing') + ' mt-2'" @click="createImageWebP('0', imageType, imageSource, imageName, '.webp', 'raw', null, null, null, null)">
+                            <span :class="'a-button-file -webpraw mt-2'" @click="createImageWebP('0', imageType, imageSource, imageName, '.webp', 'raw', null, null, null, null)">
                                 {{webP ? 'Znovu generovat WebP obrázek' : 'Generovat WebP obrázek'}}
                             </span>
                         </div>
@@ -149,7 +149,7 @@
                                                 <td class="o-image-version__td">{{item.suffix}}</td>
                                                 <td class="o-image-version__td -p0">
                                                     <span class="o-image-version__button">
-                                                        <span :class="'a-button-file -variant -data-type-' + imageType + ' -data-index-' + index + existImage(index, imageType, imageSource, imageName, '.webp', item.width, item.height, item.prefix, item.suffix)" @click="createImageWebP(index, imageType, imageSource, imageName, '.webp', 'resize', item.width, item.height, item.prefix, item.suffix)">Generovat</span>
+                                                        <span :class="'a-button-file -variant -data-type-' + imageType + ' -data-index-' + index + existImage(index, imageType, imageSource, imageName, '.webp', 'resize', item.width, item.height, item.prefix, item.suffix)" @click="createImageWebP(index, imageType, imageSource, imageName, '.webp', 'resize', item.width, item.height, item.prefix, item.suffix)">Generovat</span>
                                                     </span>
                                                 </td>                                            
                                             </tr>
@@ -208,6 +208,7 @@
 
         data() {
             return {
+                imageStatusValue: null,
                 mNavBreadcrumbsArray: [
                     {
                         id: 1,
@@ -1199,8 +1200,10 @@
                 }
             },
 
-            existImage(dataIndex: string, dataType: string, source: string, name: string, extension: string, width: number, height: number, prefix: string, suffix: string) {
-                const imageUrl = 'https://image.frytolnacestach.cz/storage' + source + (prefix || '') + name + '-' + (width ? width : height) + (suffix !== "-1x" ? suffix : '') + extension
+            existImage(dataIndex: string, dataType: string, source: string, name: string, extension: string, type: string, width: number, height: number, prefix: string, suffix: string) {
+                let imageUrl
+
+                imageUrl = 'https://image.frytolnacestach.cz/storage' + source + (prefix || '') + name + '-' + (width ? width : height) + (suffix !== "-1x" ? suffix : '') + extension
 
                 const xhr = new XMLHttpRequest()
                 xhr.open('HEAD', imageUrl, false)
@@ -1263,8 +1266,15 @@
             const imageAuthor = ref('')
 
             // Change class in button
-            function addClassToButton(className: string, dataType: string, dataIndex: string) {
-                const buttons = document.querySelectorAll(`.a-button-file.-data-type-${dataType}.-data-index-${dataIndex}`)
+            function addClassToButton(type: string, className: string, dataType: string, dataIndex: string) {
+                let buttons
+
+                if (type === 'webp-raw') {
+                    buttons = document.querySelectorAll(`.a-button-file.-webpraw`)
+                } else if ('webp-new') {
+                    buttons = document.querySelectorAll(`.a-button-file.-data-type-${dataType}.-data-index-${dataIndex}`)
+                }
+
                 buttons.forEach((button) => {
                     button.classList.remove('-loading')
                     button.classList.remove('-existing')
@@ -1307,21 +1317,25 @@
                 }
             }
 
-            const existImageWebpRaw = async () => {
-                const imageUrl = 'https://image.frytolnacestach.cz/storage' + imageSource.value + imageName.value + '.webP'
+
+            const existImageWebpRaw = async(dataIndex: string, dataType: string, source: string, name: string, extension: string, type: string, width: number, height: number, prefix: string, suffix: string) => {
+                const imageUrl = 'https://image.frytolnacestach.cz/storage' + source + name + '.webp'
+
                 const xhr = new XMLHttpRequest()
                 xhr.open('HEAD', imageUrl, false)
 
                 try {
                     xhr.send()
-                    
                     if (xhr.status === 200) {
                         webP.value = true
+                        addClassToButton('webp-raw', '-existing', dataType, dataIndex)
                     } else {
                         webP.value = false
+                        addClassToButton('webp-raw', '-no-existing', dataType, dataIndex)
                     }
                 } catch (error) {
                     webP.value = false
+                    addClassToButton('webp-raw', '-error', dataType, dataIndex)
                 }
             }
 
@@ -1334,12 +1348,12 @@
                 try {
                     xhr.send()
                     if (xhr.status === 200) {
-                        addClassToButton('-existing', dataType, dataIndex)
+                        addClassToButton('webp-new', '-existing', dataType, dataIndex)
                     } else {
-                        addClassToButton('-no-existing', dataType, dataIndex)
+                        addClassToButton('webp-new', '-no-existing', dataType, dataIndex)
                     }
                 } catch (error) {
-                    addClassToButton('-error', dataType, dataIndex)
+                    addClassToButton('webp-new', '-error', dataType, dataIndex)
                 }
             }
 
@@ -1369,7 +1383,7 @@
                         successForm.value = "Obrázek byl vytvořen"
 
                         if (type === "raw") {
-                            existImageWebpRaw()
+                            existImageWebpRaw('0', dataType, source, name, '.webp', 'raw', null, null, null, null)
                         } else {
                             setTimeout(() => {
                                 existImageNew(dataIndex, dataType, source, name, extension, width, height, prefix, suffix)
@@ -1410,7 +1424,7 @@
                     loadingData.value = true
                 }
 
-                existImageWebpRaw()
+                existImageWebpRaw('0', imageType.value, imageSource.value, imageName.value, '.webp', 'raw', null, null, null, null)
             })()
 
             //RETURN
