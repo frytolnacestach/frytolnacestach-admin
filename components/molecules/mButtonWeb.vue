45f4a86eb499c532bde42f626d4bf36437972aa5
frytolnacestach <frytolnacestach@gmail.com>
2024-02-08 16:43:16 +0100

    Create component mButtonWeb

---
+++ components/molecules/mButtonWeb.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="m-button-web">
+        <a class="m-button-web__link" :href="link" type="submit" target="_blank">{{ text }}</a>
+    </div>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmButtonWebComponent',
+
+        props: {
+            text: {
+                type: String,
+                required: true
+            },
+            link: {
+                type: String,
+                required: true
+            }
+        }
+    })
+</script>
