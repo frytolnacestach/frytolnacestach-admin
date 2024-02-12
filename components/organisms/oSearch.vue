23cf8eb4a32148d794ae3ac4bce247924a4c5764
frytolnacestach <frytolnacestach@gmail.com>
2024-02-13 00:37:31 +0100

    Create component oSearch

---
+++ components/organisms/oSearch.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="o-search">
+        <div class="o-search__outer">
+            <div class="o-search__inner">
+                <input class="a-input-search" type="text" v-model="searchQuery" placeholder="hledání..">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default defineComponent({
+        name: 'OrganismsoSearchComponent',
+
+        props: {
+            type: {
+                type: String,
+                required: true
+            },
+            page: {
+                type: Number,
+                required: true
+            },
+            items: {
+                type: Number,
+                required: true
+            }
+        },
+
+        setup(props) {
+            const runTimeConfig = useRuntimeConfig()
+            const searchQuery = ref('')
+            const searchResult = ref([])
+
+            const search = async () => {
+                fetch(`${runTimeConfig.public.baseURL}/search-admin?type=${props.type}&search=${searchQuery.value}&page=${props.page}&items=${props.items}`, {
+                    method: 'GET'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    searchResult.value = data
+                })
+            }
+
+            return {
+                searchQuery,
+                searchResult,
+                search
+            }
+        },
+
+        watch: {
+            searchQuery: function(newSearchQuery, oldSearchQuery) {
+                if (newSearchQuery.length >= 1) {
+                    this.search()
+                } else {
+                    this.searchResult.value = []
+                }
+            },
+            searchResult: {
+                deep: true,
+                handler(newValue, oldValue) {
+                    if (this.searchQuery.length >= 1) {
+                        this.$emit('search-result', this.searchResult)
+                    }
+                }
+            }
+        }
+    })
+</script>
