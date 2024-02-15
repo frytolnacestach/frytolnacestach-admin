864f42d329043566c0b647b8889174a200e45ace
frytolnacestach <frytolnacestach@gmail.com>
2024-02-16 00:11:18 +0100

    Search place in mInputsIDSNeighboringCountries

--- components/molecules/mInputsIDSNeighboringCountries.vue
+++ components/molecules/mInputsIDSNeighboringCountries.vue
@@ -9,8 +9,20 @@
                 </div>
                 <div class="o-form-item__group-inputs">
                     <div class="o-form-item__group-input">
-                        <mLabel name="ID" nameDB="id" perex="" type="json" :required=false />
-                        <input class="a-input -json" type="number" min="0" v-model="item.id" />
+                        <mLabel name="Název (in code ID)" nameDB="id" perex="" type="json" :required=false />
+                        <span class="a-button-reset" v-if="placeSelect[index]">
+                            <span class="a-button-reset__input" @click="removePlace(index)">Vybret jiné místo</span>
+                        </span>
+                        <input class="a-input hidden" type="number" min="0" v-model="item.id" />
+                        <input class="a-input" type="text" disabled="true" v-model="placeSelect[index]" v-if="placeSelect[index]" />
+                        <input class="a-input -json" type="text" v-model="searchQuery" v-if="!placeSelect[index]" />
+                        <div class="m-search-list-input__items" v-if="!item.id">
+                            <ul class="m-search-list-input__items">
+                                <li class="m-search-list-input__item" v-for="place in searchResult" :key="place.id" @click="selectPlace(place.id, place.name, index)">
+                                    {{ place.name }}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -45,7 +57,9 @@
 
         data() {
             return {
-                IDSneighboringCountriesArray: this.isValidJSON(this.value) ? JSON.parse(this.value) : []
+                IDSneighboringCountriesArray: this.isValidJSON(this.value) ? JSON.parse(this.value) : [],
+                loadPlaces: [],
+                placeSelect: []
             }
         },
 
@@ -66,6 +80,66 @@
             },
             removeIDSneighboringCountrieInput(index) {
                 this.IDSneighboringCountriesArray.splice(index, 1)
+            },
+            selectPlace(id, name, index) {
+                this.IDSneighboringCountriesArray[index].id = id
+                this.placeSelect[index] = name;
+                this.searchQuery = ''
+                this.searchResult = []
+            },
+            removePlace(index) {
+                this.IDSneighboringCountriesArray.splice(index, 1)
+                this.placeSelect.splice(index, 1);
+            },
+            checkExstingName() {
+                if (this.IDSneighboringCountriesArray[0] && this.IDSneighboringCountriesArray[0].id && !this.placeSelect[0]) {
+                    this.load(this.IDSneighboringCountriesArray)
+                }
+            },
+            load(ids) {
+                const runTimeConfig = useRuntimeConfig()
+                const placesID = ids.map(place => place.id) || []
+
+                fetch(`${runTimeConfig.public.baseURL}/places-states-array?showType=list&id=${placesID.join(',')}`, {
+                    method: 'GET'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    this.loadPlaces = data
+
+                    const sortedNames = ids.map(place => {
+                        const matchingPlace = this.loadPlaces.find(loadPlace => loadPlace.id === place.id)
+                        return matchingPlace ? matchingPlace.name : ''
+                    })
+
+                    this.placeSelect = sortedNames
+                })
+            }
+        },
+
+        mounted() {
+            this.checkExstingName()
+        },
+
+        setup(props) {
+            const runTimeConfig = useRuntimeConfig()
+            const searchQuery = ref('')
+            const searchResult = ref([])
+
+            const search = async () => {
+                fetch(`${runTimeConfig.public.baseURL}/search-admin?type=${props.type}&search=${searchQuery.value}&page=1&items=20`, {
+                    method: 'GET'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    searchResult.value = data
+                })
+            }
+
+            return {
+                searchQuery,
+                searchResult,
+                search
             }
         },
 
@@ -77,6 +151,13 @@
                 deep: true,
                 handler(newValue, oldValue) {
                     this.$emit('ids-neighboring-countries', this.IDSneighboringCountriesArray)
+                }
+            },
+            searchQuery: function(newSearchQuery, oldSearchQuery) {
+                if (newSearchQuery.length >= 1) {
+                    this.search()
+                } else {
+                    this.searchResult = []
                 }
             }
         }
