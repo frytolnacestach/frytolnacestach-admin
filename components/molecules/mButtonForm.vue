4eab0c727352d2daf5001fdd57fc3ec9c01f9eff
frytolnacestach <frytolnacestach@gmail.com>
2024-02-17 00:38:36 +0100

    Create mButtonForm

---
+++ components/molecules/mButtonForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="m-button-form">
+        <button class="m-button-form__input" type="submit">{{ text }}</button>
+    </div>
+</template>
+
+<script>
+    export default defineComponent({
+        name: 'MoleculesmButtonFormComponent',
+
+        props: {
+            text: {
+                type: String,
+                required: true
+            }
+        }
+    })
+</script>
