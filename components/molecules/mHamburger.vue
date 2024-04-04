6dbd616322ebf4fda1a0b643d517be89a5c9c3aa
frytolnacestach <frytolnacestach@gmail.com>
2024-04-04 09:43:34 +0200

    Delete js_m-hamburger.js

--- components/molecules/mHamburger.vue
+++ components/molecules/mHamburger.vue
@@ -14,6 +14,42 @@
 
 <script>
     export default defineComponent({
-        name: 'MoleculesmHamburgerComponent'
+        name: 'MoleculesmHamburgerComponent',
+
+        mounted() {
+            this.mHamburger()
+        },
+        methods: {
+            mHamburger() {
+                var hamburger = document.querySelector(".js_m-hamburger")
+
+                hamburger.addEventListener("click", () => {
+                    var status = hamburger.getAttribute("data-hamburger")
+
+                    if (status === "close") {
+                        hamburger.setAttribute("data-hamburger", "open")
+                        hamburger.classList.add("open")
+                        document.querySelector(".js_m-nav-main").classList.add("open")
+                        document.documentElement.classList.add("no-scroll")
+                        document.body.classList.add("no-scroll")
+                    } else {
+                        hamburger.setAttribute("data-hamburger", "close")
+                        hamburger.classList.remove("open")
+                        document.querySelector(".js_m-nav-main").classList.remove("open")
+                        document.documentElement.classList.remove("no-scroll")
+                        document.body.classList.remove("no-scroll")
+                    }
+                });
+
+                var navLinks = document.querySelectorAll(".js_m-nav-main__link")
+                navLinks.addEventListener("click", () => {
+                    hamburger.setAttribute("data-hamburger", "close")
+                    hamburger.classList.remove("open")
+                    document.querySelector(".js_m-nav-main").classList.remove("open")
+                    document.documentElement.classList.remove("no-scroll")
+                    document.body.classList.remove("no-scroll")
+                })
+            }
+        }
     })
 </script>
